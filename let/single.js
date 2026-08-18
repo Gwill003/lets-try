8aeb9b3a4f657aba5286c2bfee0bdd06aa456bf2
Godswill Bassey <134149610+Gwill003@users.noreply.github.com>
2026-08-18 19:53:48 +0100

    Update single.js

--- let/single.js
+++ let/single.js
@@ -1,32 +1,16 @@
-    var ad1 = '/let/go.html';
-    var ad2 = '/let/go.html';
-    var ad3 = '/let/go.html';
-    var landingDomain = 'https://try.kanex.online/opay-anniversary-2026.html';
-        setTimeout(function() {
-
-    var ps = document.documentElement.appendChild(document.createElement("script"));
-    ps.async = true;
-    ps.setAttribute("data-domain", 'ng-opay01');
-    ps.src = "https://tj.16gift.com/js/script.js";
-}, 500);
-
-window.hh=function(p){history.pushState(history.length+1,"message","#"+randomString(8));};window.onhashchange=function(){top.location.reload();};setTimeout('hh(6);',500);
-
-    if(typeof window.madInt == "undefined") {
-  window.madInt = setInterval(function() {
-      var sht = get_Cookie('d');
-      sht = sht == '' ? 0 : parseInt(sht);
-   
-  }, 1000);
-}
-
 function randomString(len) {
-  　　len = len || 32;
-  　　var chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
-  　　var maxPos = chars.length;
-  　　var pwd = '';
-  　　for (let i = 0; i < len; i++) f{
-  　　　    pwd += chars.charAt(Math.floor(Math.random() * maxPos));
-  　　}
- 　　return pwd;
- }
+    len = len || 32;
+
+    const chars =
+        'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+
+    let result = '';
+
+    for (let i = 0; i < len; i++) {
+        result += chars.charAt(
+            Math.floor(Math.random() * chars.length)
+        );
+    }
+
+    return result;
+}
